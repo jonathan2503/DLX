e2a476134a4c1ffef9a0e689422c0c0f3f8bb388
omega <57135515+jonathan2503@users.noreply.github.com>
2022-07-14 19:35:00 +0200

    Delete constants.vhd

--- Mem/constants.vhd
+++
@@ -1,8 +0,0 @@
-package CONSTANTS is
-
-   constant ALL_BITS   : integer := 32;	
-   constant N_BIT   : integer := 4;	
-   constant N_subdiv  : integer := 4;	
-   constant N_Block   : integer := 8;	
-   	
-end CONSTANTS;
