845b33ed5c9c23695c2d19472da53d8db6b09cfb
LorenzoMastro99 <101186444+LorenzoMastro99@users.noreply.github.com>
2022-07-15 18:22:50 +0200

    Update decode_stage.vhd

--- ID/decode_stage.vhd
+++ ID/decode_stage.vhd
@@ -1,4 +1,3 @@
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.std_logic_unsigned.all;
@@ -12,7 +11,7 @@ CLK: 		IN std_logic;
 RESET: 	    IN std_logic;
 
 
-IR:         IN std_logic_vector(31 downto 0);
+IR:         IN std_logic_vector(0 TO 31);
 --DA IR SI DEDUCONO TUTTE GLI ALTRI INPUT
 
 --CTR SIGNALS IN SLIDES
@@ -24,15 +23,15 @@ EN1: 		IN std_logic;
 --MY CTR SIGNALS
 SEL_IMM:    IN std_logic;--TO SELECT IMM
 SEL_RD:    IN std_logic; -- TO SELECT RD(2 CHOISES)
-ADD_WR: 	IN std_logic_vector(4 downto 0); --ADDRESS WRITING COMING FROM WB STAGE
-DATAIN: 	IN std_logic_vector(31 downto 0);--DATA IN COMING FROM WB STAGE
-NPC: 	    IN std_logic_vector(31 downto 0); -- NEW PROGRAM COUNTER
+ADD_WR: 	IN std_logic_vector(0 to 4); --ADDRESS WRITING COMING FROM WB STAGE
+DATAIN: 	IN std_logic_vector(0 to 31);--DATA IN COMING FROM WB STAGE
+NPC: 	    IN std_logic_vector(0 to 31); -- NEW PROGRAM COUNTER
 
-A: 	        OUT std_logic_vector(31 downto 0);
-B: 	        OUT std_logic_vector(31 downto 0);
-IN1: 	    OUT std_logic_vector(31 downto 0); --NPC
-IN2: 	    OUT std_logic_vector(31 downto 0); --PROPER IMMEDIATE
-RD:         OUT std_logic_vector(4 downto 0);
+A: 	        OUT std_logic_vector(0 to 31);
+B: 	        OUT std_logic_vector(0 to 31);
+IN1: 	    OUT std_logic_vector(0 to 31); --NPC
+IN2: 	    OUT std_logic_vector(0 to 31); --PROPER IMMEDIATE
+RD:         OUT std_logic_vector(0 to 4);
 
 CALL: 		IN std_logic;
 RETURN_S: 	IN std_logic;
@@ -48,7 +47,7 @@ component windowed_rf is
 generic (M: natural := 8; --number of global
 		 N: natural := 8; --number of in/out/local
          F: natural := 4;	--number of windows
-		 NBIT : natural := 64);
+		 NBIT : natural := 32);
 
  port ( 
 	 CLK: 		IN std_logic;
@@ -58,12 +57,12 @@ generic (M: natural := 8; --number of global
 	 RD2: 		IN std_logic;
 	 WR: 		IN std_logic; 
 	 
-	 ADD_WR: 	IN std_logic_vector(log2(N * 3 + M)-1 downto 0); 
-	 ADD_RD1: 	IN std_logic_vector(log2(N * 3 + M)-1 downto 0);
-	 ADD_RD2: 	IN std_logic_vector(log2(N * 3 + M)-1 downto 0);
-	 DATAIN: 	IN std_logic_vector(NBIT-1 downto 0);
-     OUT1: 		OUT std_logic_vector(NBIT-1  downto 0);
-	 OUT2: 		OUT std_logic_vector(NBIT-1  downto 0);
+	 ADD_WR: 	IN std_logic_vector(0 to log2(N * 3 + M)-1 ); 
+	 ADD_RD1: 	IN std_logic_vector(0 to log2(N * 3 + M)-1 );
+	 ADD_RD2: 	IN std_logic_vector(0 to log2(N * 3 + M)-1 );
+	 DATAIN: 	IN std_logic_vector(0 TO NBIT-1);
+     OUT1: 		OUT std_logic_vector(0 TO NBIT-1);
+	 OUT2: 		OUT std_logic_vector(0 TO NBIT-1);
 	
 	CALL: 		IN std_logic;
 	 RETURN_S: 	IN std_logic;
@@ -74,35 +73,35 @@ end component;
 component MUX21 is
 generic(N: integer:=32);
 Port (
-A: in std_logic_vector(N-1 downto 0);
-B: in std_logic_vector(N-1 downto 0);
+A: in std_logic_vector(0 TO N-1);
+B: in std_logic_vector(0 TO N-1);
 sel: in std_logic;
-Y: out std_logic_vector(N-1 downto 0));
+Y: out std_logic_vector(0 TO N-1));
 end component;
 
 component REG is
     Generic(NBIT: integer);
 	Port  ( 
-	    D:	IN	std_logic_vector(NBIT-1 downto 0);
+	    D:	IN	std_logic_vector(0 TO NBIT-1);
 		CK:	IN	std_logic;
 		EN : IN std_logic;
 		RESET:	IN	std_logic;
-		Q:	OUT	std_logic_vector(NBIT-1 downto 0));
+		Q:	OUT	std_logic_vector(0 TO NBIT-1));
 end component;
 
 COMPONENT sign_extender_16 is
-Port (A: in std_logic_vector(15 downto 0);
-      B: out std_logic_vector(31 downto 0));
+Port (A: in std_logic_vector(0 to 15);
+      B: out std_logic_vector(0 to 31));
 end COMPONENT;
 
 
 COMPONENT sign_extender_26 is
-Port (A: in std_logic_vector(25 downto 0);
-      B: out std_logic_vector(31 downto 0));
+Port (A: in std_logic_vector(0 to 25);
+      B: out std_logic_vector(0 to 31));
 end COMPONENT;
 
-SIGNAL RD_EXT: std_logic_vector(4 downto 0);
-SIGNAL IMM1_EXT, IMM2_EXT,IMM_EXT,OUT1,OUT2: std_logic_vector(31 downto 0);
+SIGNAL RD_EXT: std_logic_vector(0 to 4);
+SIGNAL IMM1_EXT, IMM2_EXT,IMM_EXT,OUT1,OUT2: std_logic_vector(0 to 31);
 
 
 
@@ -111,11 +110,11 @@ IN1 <= NPC;
 
 
 EX1: sign_extender_16 PORT MAP(
-                                A => IR(31 DOWNTO 16),
+                                A => IR(16 TO 31),
                                 B =>IMM1_EXT);
                                 
 EX2: sign_extender_26 PORT MAP(
-                                A => IR(31 DOWNTO 6),
+                                A => IR(6 TO 31),
                                 B =>IMM2_EXT);                          
                                 
 MUX_1: MUX21 PORT MAP (
@@ -126,13 +125,13 @@ MUX_1: MUX21 PORT MAP (
                         
 MUX_2: MUX21 GENERIC MAP(N=>5)
              PORT MAP (
-                        A=>IR(15 DOWNTO 11),
-                        B=>IR(20 DOWNTO 16),
+                        A=>IR(11 TO 15),
+                        B=>IR(16 TO 20),
                         SEL=>SEL_RD,
                         Y=> RD_EXT);     
                                            
                         
-R1: REG GENERIC MAP(NBIT=>4)
+R1: REG GENERIC MAP(NBIT=>5)
         PORT MAP( D => RD_EXT,
                   CK => CLK,
                   EN => EN1,
@@ -164,7 +163,7 @@ WR: windowed_rf GENERIC MAP (
                                 M=> 8,
 		                        N=>8,
                                 F=> 4,
-		                        NBIT=>64)
+		                        NBIT=>32)
 		        PORT MAP (
 	CLK => CLK,
      RESET=>RESET,
@@ -173,8 +172,8 @@ WR: windowed_rf GENERIC MAP (
 	 RD2=>RF2,
 	 WR=>WF1,
 	 ADD_WR=>ADD_WR, 
-	 ADD_RD1 => IR(10 DOWNTO 6),
-	 ADD_RD2 => IR(15 DOWNTO 11),
+	 ADD_RD1 => IR(6 TO 10),
+	 ADD_RD2 => IR(11 TO 15),
 	 DATAIN => DATAIN,
      OUT1 => OUT1,
 	 OUT2 => OUT2,
@@ -186,3 +185,4 @@ WR: windowed_rf GENERIC MAP (
                   
 
 end Behavioral;
+
