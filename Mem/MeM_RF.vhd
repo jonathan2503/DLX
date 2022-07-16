a071435d5a81dce1a5896044f382ad9a95b3c88a
omega <57135515+jonathan2503@users.noreply.github.com>
2022-07-16 13:56:51 +0200

    Add files via upload

---
+++ Mem/MeM_RF.vhd
@@ -0,0 +1,45 @@
+library ieee; 
+use ieee.std_logic_1164.all; 
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+
+
+entity rf is 
+port (
+	  RF_i: in  std_logic_vector (31 downto 0);
+	  en  : in  std_logic ;
+	  Rf_o: out std_logic_vector (31 downto 0));	
+	  -- clk: in std_logic;		
+end rf ; 
+
+
+architecture behevioural of rf is
+	subtype word is std_logic_vector(31 downto 0);
+begin
+	
+	process (RF_i , en )  is 
+	variable aka : word ;
+	begin
+
+
+
+    if ( RF_i(0) = 'Z' ) then
+	         if (en = '0') then
+			Rf_o <= ( others => 'Z') ;
+			else
+			Rf_o <= aka ;
+			end if ;
+	else
+	    aka :=  RF_i ; 
+		if (en = '1') then
+			Rf_o <= aka ;
+		else 
+			Rf_o <= ( others => 'Z') ;
+		end if;
+	end if;
+
+	end process;
+
+end behevioural;
+	
