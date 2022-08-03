7474c5a3ee57957269589354136cb02ea750ee3f
LorenzoMastro99 <101186444+LorenzoMastro99@users.noreply.github.com>
2022-08-03 16:09:55 +0200

    Update BC_adder.vhd

--- 03_EXE/BC_adder.vhd
+++ 03_EXE/BC_adder.vhd
@@ -2,199 +2,130 @@
 --                                            HIGH MATE                                           --
 ----------------------------------------------------------------------------------------------------
 
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.all;
-
-package high_math is 
-	function divide(n:integer; m:integer) return integer;
-	function log2(n:integer) return integer;
-end high_math;
-
-package body high_math is
-	function divide(n:integer; m:integer) return integer is
-	begin
-		if(n mod m) = 0 then
-			return n/m;
-		else
-			return (n/m)+1;
-		end if;
-	end divide;
-	
-	function log2(n:integer) return integer is
-	begin
-		if n <= 2 then
-			return 1;
-		else
-			return 1 + log2(divide(n,2));
-		end if;
-	end log2;
-end high_math;
-
-
-----------------------------------------------------------------------------------------------------
---                                             FA                                                --
-----------------------------------------------------------------------------------------------------
 library ieee; 
 use ieee.std_logic_1164.all; 
-
-entity FA is 
-       
-	Port (	A:	In	std_logic;
-		B:	In	std_logic;
-		Ci:	In	std_logic;
-		S:	Out	std_logic;
-		Co:	Out	std_logic);
-end FA; 
-
-architecture BEHAVIORAL of FA is
-
-begin
-
-  S <= A xor B xor Ci ;
-  Co <= (A and B) or (B and Ci) or (A and Ci) ;
-  
-  
-end BEHAVIORAL;
-
-configuration CFG_FA_BEHAVIORAL of FA is
-  for BEHAVIORAL 
-  end for;
-end CFG_FA_BEHAVIORAL;
-
-
-----------------------------------------------------------------------------------------------------
---                                             G                                                  --
-----------------------------------------------------------------------------------------------------
-library ieee; 
-use ieee.std_logic_1164.all; 
-use ieee.std_logic_1164.all; 
-
-entity g is 
-       
-	Port (	
-	    Ga:	In	std_logic;
-		Gb:	In	std_logic;
-		P:	In	std_logic;
-		G_out:	Out	std_logic
-		);
-end g; 
-
-architecture BHE of g is
-
-begin
-
-   G_out <= Ga  or (P and Gb) ;
-  
-  
-end BHE;
-
-
-configuration CFG_G_BHE of g is
-  for BHE
-  end for;
-end CFG_G_BHE;
-
-
-----------------------------------------------------------------------------------------------------
---                                             MUX                                                --
-----------------------------------------------------------------------------------------------------
-
-library IEEE;
-use IEEE.Std_logic_1164.all;
-use IEEE.Numeric_Std.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
 use WORK.constants.all;
 
-entity  mux  IS
-    	GENERIC(
-		NBIT : integer := N_BIT
-	            );
-		     
-	Port (	A:   	In	std_logic_vector(NBIT -1 downto 0) ;
-	    	B:	    In	std_logic_vector(NBIT -1  downto 0);
-	    	SEL:	In	std_logic;
-	    	Y:	Out	std_logic_vector(NBIT-1  downto 0));
+entity SUM_GENERATOR is 
 
-    end mux;
+		generic (
+			NBIT_PER_BLOCK: integer := N_BIT;
+			NBLOCKS:	integer := N_Block);
+		port (
+			A:	in	std_logic_vector(NBIT_PER_BLOCK*NBLOCKS-1 downto 0);
+			B:	in	std_logic_vector(NBIT_PER_BLOCK*NBLOCKS-1 downto 0);
+			Ci:	in	std_logic_vector(NBLOCKS-1 downto 0);
+			S:	out	std_logic_vector(NBIT_PER_BLOCK*NBLOCKS-1 downto 0));
+end SUM_GENERATOR; 
 
-architecture Behav of mux is
+architecture STRUCTURAL of SUM_GENERATOR is
 
+	component csb --define block adder
+		generic ( ANBIT : integer := NBIT_PER_BLOCK
+	);
+	
+	Port (
+		A_c:	In	std_logic_vector(ANBIT-1 downto 0);
+		B_c:	In	std_logic_vector(ANBIT-1 downto 0);
+		Ci_c:	In	std_logic;
+		So_c:	Out	std_logic_vector(ANBIT-1 downto 0));
+   
+
+  end component; 
+
+  --signal S_TMP : std_logic_vector(NBIT_PER_BLOCK*NBLOCKS-1  downto 0);-- buffer somma temporanea
+ -- signal C_TMP : std_logic_vector(NBLOCKS-1  downto 0);               -- buffer di tutti i carry temporanei
+  
 begin
+      blo:  for I in 1 to NBLOCKS  generate
+       cbl : csb
+			--generic map (ANBIT => NBIT_PER_BLOCK)
+			Port Map ( 
+				A(NBIT_PER_BLOCK*I-1 downto NBIT_PER_BLOCK*(I-1)), 
+				B(NBIT_PER_BLOCK*I-1 downto NBIT_PER_BLOCK*(I-1)), 
+				Ci(I-1), S(NBIT_PER_BLOCK*I-1 downto NBIT_PER_BLOCK*(I-1))
+			); 
+            end generate;
+			
+end STRUCTURAL;
 
-process (A, B, SEL)
-
-begin
-       if Sel = '1' then
-       
-       
-       	  for I in 0 to NBIT-1 loop
-          Y(I) <= A(I);
-		  end loop;
-       
-               
-       else   
-       	  for I in 0 to NBIT-1 loop
-          Y(I) <= B(I);
-		  end loop;
-       end if;
-
-end process;
-End Behav ;
-
-configuration CFG_MUX of mux is
-  for Behav
+configuration CFG_SUM_GENERATOR_STRUCTURAL of SUM_GENERATOR is
+  for STRUCTURAL 
+	for blo
+		for all : csb
+			use configuration work.CFG_CSB_STRUCTURAL;
+		end for;
+	end for;
   end for;
-end CFG_MUX;
+end CFG_SUM_GENERATOR_STRUCTURAL;
 
 
+------------------------------------------------------------------------------------------------------------------------------------------------
+--                                              sparse tree look-ahead adder                                                                  --
+------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-----------------------------------------------------------------------------------------------------
---                                             F_add                                              --
-----------------------------------------------------------------------------------------------------
 library ieee; 
 use ieee.std_logic_1164.all; 
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+use WORK.constants.all;
+use work.high_math.all;
 
-entity f_add is 
-       
+entity CARRY_GENERATOR is 
+
+   GENERIC(
+		NBIT : integer := ALL_BITS;
+		NBIT_PER_BLOCK: integer := N_subdiv
+	);
+	
+	Port (
+		A:	In	std_logic_vector(NBIT-1 downto 0);
+		B:	In	std_logic_vector(NBIT-1  downto 0);
+		Cin:	In	std_logic;
+		Co:	Out	std_logic_vector(NBIT/NBIT_PER_BLOCK downto 0)
+	);
+end CARRY_GENERATOR; 
+
+
+architecture STRUCTURAL of CARRY_GENERATOR is
+
+ 
+ ---------------- FIRST adder of basic adder---------------------------
+  component f_add  
 	Port (	
 	    f_a:	In	std_logic;  --variable of upper block left
 		f_b:	In	std_logic;  --variable of upper block reight
 		G:	Out	std_logic;  --output of first
 		P:	Out	std_logic  --output of second
 		);
-end f_add; 
-
-architecture bhe of f_add is
-
-begin
-
-   G <= f_a AND f_b ;
-   P <= f_a XOR f_b;
+	
+  end component; 
+  
+  ----------------END FIRST adder of basic adder------------------------
+  
+ 
+ 
+ 
+---------------------- g adder ----------------------------------------- 
+  component g 
+	Port (	
+	    Ga:	In	std_logic;
+		Gb:	In	std_logic;
+		P:	In	std_logic;
+		G_out:	Out	std_logic
+		);
+  end component; 
+  
+ ---------------END g --------------------------------- 
   
   
-end bhe;
+  
+ 
+ ---------------------- pg adder ----------------------------------------  
+  component pg 
 
-
-configuration CFG_F_ADD_STRUCTURAL of f_add is
-  for bhe
-  end for;
-end CFG_F_ADD_STRUCTURAL;
-
-
-
-
-
-----------------------------------------------------------------------------------------------------
---                                            PG                                                  --
-----------------------------------------------------------------------------------------------------
-
-
-library ieee; 
-use ieee.std_logic_1164.all; 
-
-entity pg is 
-       
 	Port (	
 	    Ga:	In	std_logic;  --variable of upper block left
 		Gb:	In	std_logic;  --variable of upper block reight
@@ -203,25 +134,102 @@ entity pg is
 		G_out:	Out	std_logic;  --output of first
 		P_out:	Out	std_logic  --output of second
 		);
-end pg; 
-
-architecture BHE of pg is
-
-begin
-
-   G_out <= Ga  or (P1 and Gb) ;
-   P_out <= P1 and P2  ;
-   --P_out <= P1 and P2  ;
+  end component; 
   
-  
-end BHE;
+ ---------------END g adderbasic adder--------------------------------- 
+ type SignalVector is array(NBIT downto 1) of std_logic_vector(NBIT downto 1);
+ signal P_1 : std_logic_vector(NBIT downto 1);
+ signal G_1 : std_logic_vector(NBIT downto 1);
+ signal C_G : SignalVector;
+ signal C_P : SignalVector;
+ constant layers : integer := log2(NBIT);
+ 
+ begin
+	--step1 get pg net work
+	R0: for i in 0 to NBIT-1  generate
+         R0_0: f_add Port Map ( f_a=> A(i),f_b => B(i) , G =>C_G(i+1)(i+1), P =>C_P(i+1)(i+1));
+    end generate R0;
+	Co(0) <= Cin;
+	
+		R1: for i in 1 to layers generate
+			R2: for j in 1 to NBIT generate
+				-- get G block
+				IFG: if ((j mod 2**i = 0 or j mod NBIT_PER_BLOCK = 0) and j> 2**(i-1) and j <= 2**i) generate
+					G_B: g 
+						Port Map(
+							Ga => C_G(j)(2**(i-1)+1),
+							Gb => C_G(2**(i-1))(1),
+							P  => C_P(j)(2**(i-1)+1),						
+							G_out => C_G(j)(1)
+							
+						);
+					res1: if j mod NBIT_PER_BLOCK = 0 generate
+						Co(j/NBIT_PER_BLOCK) <= C_G(j)(1);
+					end generate res1;
+				end generate IFG;
+				IFP0: if j/2**i >= 1 and j mod 2**i > 0 and ((j - 2**i) > 2**(i-1)) and j < 2**(i+1)and (j mod NBIT_PER_BLOCK = 0)generate
+					PG_B0: pg
+						Port Map(
+							Ga => C_G(j)((j/2**(i-1))*2**(i-1)+1),
+							Gb => C_G((j/2**(i-1))*2**(i-1))((j/2**(i-1))*2**(i-1)-2**(i-1)+1),
+							P1 => C_P(j)((j/2**(i-1))*2**(i-1)+1),
+							P2 => C_P((j/2**(i-1))*2**(i-1))((j/2**(i-1))*2**(i-1)-2**(i-1)+1),
+							G_out => C_G(j)((j/2**(i-1))*2**(i-1)-2**(i-1)+1),
+							P_out => C_P(j)((j/2**(i-1))*2**(i-1)-2**(i-1)+1)
+						);
+				end generate IFP0;
+				ifP1: if j/2**i > 1 and j mod 2**i =0 generate
+					PG_B1:pg
+						Port Map(
+							Ga => C_G(j)(j-2**(i-1)+1),
+							Gb => C_G(j-2**(i-1))(j-2**i+1),
+							P1 => C_P(j)(j-2**(i-1)+1),
+							P2 => C_P(j-2**(i-1))(j-2**i+1),
+							G_out => C_G(j)(j-2**i+1),
+							P_out => C_P(j)(j-2**i+1)
+						);
+				end generate IFP1;
+			end generate R2;
+		end generate R1;
+end STRUCTURAL;
 
 
-
-configuration CFG_PG_BHE of pg is
-  for BHE
+configuration CFG_CARRY_GENERATOR_STRUCTURAL of CARRY_GENERATOR is
+  for STRUCTURAL
+	for R0
+		for all : f_add
+			use configuration WORK.CFG_F_ADD_STRUCTURAL;
+		end for;
+	end for;
+	for R1
+		for R2 
+			for IFG
+				for all : g
+					use configuration WORK.CFG_G_BHE;
+				end for;
+			end for;
+			for IFP0
+				for all : pg
+					use configuration WORK.CFG_PG_BHE;
+				end for;
+			end for;
+			for IFP1
+				for all : pg
+					use configuration WORK.CFG_PG_BHE;
+				end for;
+			end for;
+		end for;
+	end for;
   end for;
-end CFG_PG_BHE;
+end CFG_CARRY_GENERATOR_STRUCTURAL;
+
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+--                                              P4 ADDER                                                               --
+------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 
 
 library ieee; 
@@ -230,73 +238,112 @@ use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
 use WORK.constants.all;
 
-entity RCA is 
+
+entity P4_ADDER is 
 
    GENERIC(
-		NBIT : integer := N_BIT
-	            );
+		NBIT : integer := ALL_BITS 
+	);
 	
 	Port (
 		A:	In	std_logic_vector(NBIT-1 downto 0);
 		B:	In	std_logic_vector(NBIT-1  downto 0);
-		Ci:	In	std_logic;
-		Co:	out	std_logic;
-		So:	Out	std_logic_vector(NBIT-1  downto 0));
-end RCA; 
+		Cin:	In	std_logic;
+		S:	Out	std_logic_vector(NBIT-1 downto 0);
+		Cout:	Out	std_logic
+	);
+		
+		
+end P4_ADDER; 
 
 
-----------------------------------------------------------------------------------------------------
---                                           RCA                                                  --
-----------------------------------------------------------------------------------------------------
+
+architecture STRUCTURAL of P4_ADDER is
 
 
-architecture STRUCTURAL of RCA is
+component SUM_GENERATOR 
 
-  signal S_TMP : std_logic_vector(NBIT-1  downto 0);-- buffer somma temporanea
-  signal C_TMP : std_logic_vector(NBIT  downto 0);-- buffer di tutti i carry temporanei
+		generic (
+			NBIT_PER_BLOCK: integer := N_BIT;
+			NBLOCKS:	integer := N_Block
+		);
+		port (
+			A:	in	std_logic_vector(NBIT_PER_BLOCK*NBLOCKS-1 downto 0);
+			B:	in	std_logic_vector(NBIT_PER_BLOCK*NBLOCKS-1 downto 0);
+			Ci:	in	std_logic_vector(NBLOCKS-1 downto 0);
+			S:	out	std_logic_vector(NBIT_PER_BLOCK*NBLOCKS-1 downto 0)
+		);
+end component; 
 
-  component FA --define full adder
 
-  Port (
-     A:	    In	std_logic;
-	 B:  	In	std_logic;
-	 Ci:	In	std_logic;
-	 
-	 S:	Out	std_logic;
-	 Co:	Out	std_logic);
-  end component; 
 
+
+component CARRY_GENERATOR 
+   GENERIC(
+		NBIT : integer := NBIT;
+		NBIT_PER_BLOCK: integer := N_subdiv
+	);
+	
+	Port (
+		A:	In	std_logic_vector(NBIT-1 downto 0);
+		B:	In	std_logic_vector(NBIT-1  downto 0);
+		Cin:	In	std_logic;
+		
+		Co:	Out	std_logic_vector(NBIT/NBIT_PER_BLOCK downto 0)
+	);
+end component; 
+
+
+
+ signal C_TMP : std_logic_vector(NBIT/N_subdiv downto 0);-- buffer somma temporanea
+ signal A_TMP : std_logic_vector(NBIT-1 downto 0);-- buffer somma temporanea
+ signal B_TMP : std_logic_vector(NBIT-1 downto 0);-- buffer somma temporanea
+ -- signal C_TMP : std_logic_vector(NBLOCKS-1  downto 0);               -- buffer di tutti i carry temporanei
+  
 begin
 
-  C_TMP(0) <= Ci; --structure | - , - ,- ,- , Ci |
-  -- S £ Co ->original pos
-  
-  
-  ADDER1:  for I in 1 to NBIT  generate
-     FAI : FA  
-     Port Map (A(I-1), B(I-1), C_TMP(I-1), S_TMP(I-1), C_TMP(I)); 
-  end generate;
 
-  So <= S_TMP;       --associaton if stemp to    S
-  Co <= C_TMP(NBIT );    --output last element     | Co , - ,- ,- , - |
+	UCARRY : CARRY_GENERATOR
+		--GENERIC Map (
+		--	NBIT => NBIT,
+		--	NBIT_PER_BLOCK => N_subdiv
+		--)
+		Port Map(
+			A => A, 
+			B => B, 
+			Cin =>Cin, 
+			Co => C_TMP
+		);
+		
+	USUM : SUM_GENERATOR
+		--GENERIC Map (
+        --   NBIT_PER_BLOCK => N_BIT,
+		--    NBLOCKS	=> N_Block 
+		--)
+		Port Map ( 
+			A => A,
+            B => B,
+            Ci =>c_TMP(NBIT/N_subdiv-1 downto 0),
+			S => S 
+		);		
 
-
+	Cout <= c_TMP(NBIT/N_subdiv);
+      
 end STRUCTURAL;
 
-configuration CFG_RCA_STRUCTURAL of RCA is
+configuration CFG_P4_ADDER_STRUCTURAL of P4_ADDER is
   for STRUCTURAL 
-    for ADDER1
-      for all : FA
-        use configuration WORK.CFG_FA_BEHAVIORAL;
-      end for;
+	for all : CARRY_GENERATOR
+		use configuration work.CFG_CARRY_GENERATOR_STRUCTURAL;
+	end for;
+	for all : SUM_GENERATOR
+		use configuration work.CFG_SUM_GENERATOR_STRUCTURAL;
 	end for;
   end for;
-end CFG_RCA_STRUCTURAL;
-
-
-----------------------------------------------------------------------------------------------------
---                                           CSB                                                  --
-----------------------------------------------------------------------------------------------------
+end CFG_P4_ADDER_STRUCTURAL;
+-----------------------------------------------------------------------------------
+                                           --MUX21
+-----------------------------------------------------------------------------------       
 
 library ieee; 
 use ieee.std_logic_1164.all; 
@@ -304,72 +351,105 @@ use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
 use WORK.constants.all;
 
-entity csb is 
-    GENERIC(
-		ANBIT : integer := N_BIT
-	            );
+entity MUX21 is
+ GENERIC(
+		NBIT : integer := ALL_BITS 
+	);
+	
+port(
+A: IN std_logic_vector( NBIT -1 DOWNTO 0);
+B: IN std_logic_vector( NBIT -1 DOWNTO 0);
+S: IN std_logic;
+Y: OUT std_logic_vector( NBIT -1 DOWNTO 0)
+);
+END entity;
+
+architecture BEHAV of MUX21 is
+begin
+
+process(A,B,S)
+begin
+if (S = '0') then
+Y <= a;
+elsif (S = '1')then
+y <= b;
+end if;
+end process;
+end BEHAV;
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+--                                              P4 ADDER/SUBTRACTOR                                                               --
+------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+library ieee; 
+use ieee.std_logic_1164.all; 
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+use WORK.constants.all;
+
+
+entity P4_ADDER_SUB is 
+
+   GENERIC(
+		NBIT : integer := ALL_BITS 
+	);
 	
 	Port (
-		A_c:	In	std_logic_vector(ANBIT-1 downto 0);
-		B_c:	In	std_logic_vector(ANBIT-1 downto 0);
-		Ci_c:	In	std_logic;
-		So_c:	Out	std_logic_vector(ANBIT-1 downto 0));
-end csb; 
-
-
-
-
-
-architecture STRUCTURAL of csb is
-
-  signal S_TMP_Ba : std_logic_vector(N_BIT-1 downto 0);-- buffer bA temporanea
-  signal S_TMP_Bb : std_logic_vector(N_BIT-1 downto 0);-- buffer bB temporanea
-
-  component RCA 
-	GENERIC(
-		NBIT : integer := N_BIT
+		A:	In	std_logic_vector(NBIT-1 downto 0);
+		B:	In	std_logic_vector(NBIT-1  downto 0);
+		ADD_SUB: In std_logic;
+		Cin:	In	std_logic;
+		S:	Out	std_logic_vector(NBIT-1 downto 0);
+		Cout:	Out	std_logic
 	);
+		
+		
+end P4_ADDER_SUB; 
+
+
+
+architecture STRUCTURAL of P4_ADDER_SUB is
+
+component P4_ADDER is 
+
+   GENERIC(
+		NBIT : integer := ALL_BITS 
+	);
+	
 	Port (
-		A:	In	std_logic_vector(N_BIT-1 downto 0);
-		B:	In	std_logic_vector(N_BIT-1 downto 0);
-		Ci:	In	std_logic;
-		So:	Out	std_logic_vector(N_BIT-1 downto 0)
+		A:	In	std_logic_vector(NBIT-1 downto 0);
+		B:	In	std_logic_vector(NBIT-1  downto 0);
+		Cin:	In	std_logic;
+		S:	Out	std_logic_vector(NBIT-1 downto 0);
+		Cout:	Out	std_logic
 	);
-  end component; 
-  
-    component mux
-	Port (	A:   	In	std_logic_vector(N_BIT-1 downto 0) ;
-	    	B:	    In	std_logic_vector(N_BIT-1 downto 0);
-	    	SEL:	In	std_logic;
-	    	Y:	Out	std_logic_vector(N_BIT-1 downto 0)
-		);
-	    	
-   end component; 
-  
-  
-  
+		
+		
+end component; 
 
+
+component MUX21 is
+ GENERIC(
+		NBIT : integer := ALL_BITS 
+	);
+	
+port(
+A: IN std_logic_vector( NBIT -1 DOWNTO 0);
+B: IN std_logic_vector( NBIT -1 DOWNTO 0);
+S: IN std_logic;
+Y: OUT std_logic_vector( NBIT -1 DOWNTO 0)
+);
+END component;
+
+Signal B_2: std_logic_vector( NBIT -1 DOWNTO 0);
 begin
-       Ba: RCA
-       --generic map (NBIT =>N_BIT)
-       port map (	A => A_c  ,B=> B_c , Ci=> '1', So=> S_TMP_Ba);
-       Bb: RCA 
-	   --generic map (NBIT =>N_BIT)
-	   port map (	A => A_c  ,B=> B_c , Ci=> '0', So=> S_TMP_Bb);
-       Mu: mux 
-	   --generic map (NBIT =>N_BIT)
-	   port map (	A => S_TMP_Ba ,B=> S_TMP_Bb , SEL=> Ci_c , Y=> So_c);
-       
+m: MUX21 port map (B, std_logic_vector(unsigned(NOT(B))+1),ADD_SUB,B_2);
+a2: P4_ADDER PORT MAP (A,B_2,Cin,S,Cout);
+      
 end STRUCTURAL;
 
 
-configuration CFG_CSB_STRUCTURAL of csb is
-  for STRUCTURAL
-    for all:RCA
-      use configuration WORK.CFG_RCA_STRUCTURAL;
-    end for;
-	for all: mux
-	  use configuration WORK.CFG_MUX;
-	end for;
-  end for;
-end CFG_CSB_STRUCTURAL;
+
